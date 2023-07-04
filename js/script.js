d2d0297ae317a1f103c41b4afbd6f338534fcb51
Ivan <ivanfilippovw@gmail.com>
2023-07-04 11:52:37 +0400

    fix: naming and js mistakes

--- js/script.js
+++ js/script.js
@@ -1,75 +1,74 @@
-// Переключение темы
+/**
+ * Переключение темы
+*/
+const lightThemeButtonElement = document.querySelector(".light-theme-button");
+const darkThemeButtonElement = document.querySelector(".dark-theme-button");
 
-let lightThemeButton = document.querySelector(".theme-button-light");
-let darkThemeButton = document.querySelector(".theme-button-dark");
-
-lightThemeButton.onclick = function () {
-  lightThemeButton.classList.add("active");
-  darkThemeButton.classList.remove("active");
-  document.body.classList.remove("dark");
+lightThemeButtonElement.onclick = function () {
+  lightThemeButtonElement.classList.add("is-active");
+  darkThemeButtonElement.classList.remove("is-active");
+  document.body.classList.remove("is-dark");
 }
 
-darkThemeButton.onclick = function () {
-  darkThemeButton.classList.add("active");
-  lightThemeButton.classList.remove("active");
-  document.body.classList.add("dark");
+darkThemeButtonElement.onclick = function () {
+  darkThemeButtonElement.classList.add("is-active");
+  lightThemeButtonElement.classList.remove("is-active");
+  document.body.classList.add("is-dark");
 }
 
-
-
-// Переключение шрифта
-
+/**
+ * Переключение шрифта
+ */
 let sansSerifButton = document.querySelector(".font-button-sans-serif");
 let serifButton = document.querySelector(".font-button-serif");
 
 sansSerifButton.onclick = function () {
-  sansSerifButton.classList.add("active");
-  serifButton.classList.remove("active");
-  document.body.classList.remove("serif");
+  sansSerifButton.classList.add("is-active");
+  serifButton.classList.remove("is-active");
+  document.body.classList.remove("is-serif");
 }
 
 serifButton.onclick = function () {
-  serifButton.classList.add("active");
-  sansSerifButton.classList.remove("active");
-  document.body.classList.add("serif");
+  serifButton.classList.add("is-active");
+  sansSerifButton.classList.remove("is-active");
+  document.body.classList.add("is-serif");
 }
 
+/**
+ * Сокращенный текст статьи > укороченный
+ */
+let blogArticleElements = document.querySelectorAll(".blog-article");
 
+for (let blogArticleElement of blogArticleElements) {
+  let moreButton = blogArticleElement.querySelector(".more");
 
-// Сокращенный текст статьи > укороченный
-
-let blogArticles = document.querySelectorAll(".blog-article");
-
-for (let blogArticle of blogArticles) {
-  let moreButton = blogArticle.querySelector(".more");
-  moreButton.onclick = function (evt) {
-    blogArticle.classList.remove("short");
+  moreButton.onclick = function () {
+    blogArticleElement.classList.remove("short");
   };
 }
 
-
-
-// Переключение раскладки карточек
-
+/**
+ * Переключение раскладки карточек
+ */
 let gridButton = document.querySelector(".card-view-button-grid");
 let listButton = document.querySelector(".card-view-button-list");
 let cardsList = document.querySelector(".rent__container");
 
 gridButton.onclick = function () {
   cardsList.classList.remove("list");
-  gridButton.classList.add("active");
-  listButton.classList.remove("active");
+  gridButton.classList.add("is-active");
+  listButton.classList.remove("is-active");
 }
 
 listButton.onclick = function () {
   cardsList.classList.add("list");
-  listButton.classList.add("active");
-  gridButton.classList.remove("active");
+  listButton.classList.add("is-active");
+  gridButton.classList.remove("is-active");
 }
 
-
-
-// Галерея
+/**
+ * Галерея
+ */
 let activePhoto = document.querySelector(".gallery__active-img");
 let previews = document.querySelectorAll(".gallery__list a");
 
